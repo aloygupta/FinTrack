54e4dfd893bb7249239fb813cbc1c9ec09bdac80
Aloy Gupta <aloygupta1993@gmail.com>
2018-03-28 20:43:32 +0530

    R import fix

--- app/app/src/main/java/io/github/aloygupta/fintrack/onboarding/OnboardingActivity.java
+++ app/app/src/main/java/io/github/aloygupta/fintrack/onboarding/OnboardingActivity.java
@@ -3,6 +3,8 @@ package io.github.aloygupta.fintrack.onboarding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.github.aloygupta.fintrack.R;
+
 public class OnboardingActivity extends AppCompatActivity {
 
     @Override
