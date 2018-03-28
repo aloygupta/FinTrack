a91aac4a0b2b94d895d157ae78e2a633052237e7
Aloy Gupta <aloygupta1993@gmail.com>
2018-03-28 20:54:07 +0530

    init setup

--- app/app/src/main/java/io/github/aloygupta/fintrack/utils/SharedPrefUtils.java
+++ app/app/src/main/java/io/github/aloygupta/fintrack/utils/SharedPrefUtils.java
@@ -2,6 +2,7 @@ package io.github.aloygupta.fintrack.utils;
 
 /**
  * Created by aloy on 3/28/2018.
+ * Class to handle operations related to SharedPreferences
  */
 public class SharedPrefUtils {
 }
